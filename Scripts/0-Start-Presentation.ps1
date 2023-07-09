0e84482125f6f4ec26c8540d7d3d127f5920493a
Jay <honeycutt.james@gmail.com>
2023-07-09 13:07:59 -0400

    Added Event logs to parse

--- Scripts/0-Start-Presentation.ps1
+++ Scripts/0-Start-Presentation.ps1
@@ -1,4 +1,9 @@
 # Start with downloading the appropriate software
-code .\Get-Software.ps1
-code .\DemoScripts\ICMP-Test.ps1
+code ".\Scripts\1-Get-Software.ps1"
+
+# PowerShell Remoting
+code ".\Scripts\2-PowerShell-Remoting.ps1"
+
+# Log Parsing
+code ".\Scripts\3-PowerShell-LogParsing.ps1"
 
