bde8a28cf9ebb9c3552bb5cd3f6ad2acbf793029
Jay <honeycutt.james@gmail.com>
2023-07-09 13:10:18 -0400

    Added more logs

---
+++ Scripts/3-PowerShell-LogParsing.ps1
@@ -0,0 +1,8 @@
+###########################################################################
+# Setup
+###########################################################################
+
+# Event ID cheetsheet
+https://www.ultimatewindowssecurity.com/securitylog/encyclopedia
+
+
